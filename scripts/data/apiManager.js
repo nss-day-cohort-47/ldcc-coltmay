b695b45dde71c59afe964632b96cfc141b63aab8
ColtMay <colten_mayberry@pm.me>
2021-03-29 14:49:15 -0500

    FINALLY have toppings with comma separated values.  thanks Chad!

--- scripts/data/apiManager.js
+++ scripts/data/apiManager.js
@@ -1,6 +1,6 @@
 const apiURL = "http://localhost:8088";
 
-//// user functions
+// user functions
 let loggedInUser = {}
 
 export const getLoggedInUser = () => {
@@ -46,16 +46,17 @@ export const registerUser = (userObj) => {
 }
 
 
-///// snack functions
+// snack functions
 
 let snackCollection = [];
+let toppingCollection = [];
 
 export const useSnackCollection = () => {
   //Best practice: we don't want to alter the original state, so
   //make a copy of it and then return it
   //the spread operator makes quick work
-  const snackCollectionCopy = [...snackCollection]
-  return snackCollectionCopy;
+	const snackCollectionCopy = [...snackCollection]
+	return snackCollectionCopy;
 }
 
 export const getSnacks = () => {
@@ -67,7 +68,29 @@ export const getSnacks = () => {
 		})
 }
 
+//* A function that will fetch a single snack from the snack database by ID, with expanded properties.
 export const getSingleSnack = (snackId) => {
-	return fetch(`${apiURL}/snacks/${snackId}/?_expand=type&_expand=inFlavor&_expand=season&_expand=shape`)
+	return fetch(`${apiURL}/snacks/${snackId}/?_expand=type&_expand=inFlavor&_expand=season&_expand=shape&_embed=snackToppings`)
+	.then(response => response.json())
+	.then(parsedResponse => {
+		return getSingleSnackToppings(snackId)
+			.then((toppings) => {
+				parsedResponse.toppings = toppings;
+				return parsedResponse;
+			})
+	})
+}
+
+export const getSingleSnackToppings = (snackId) => {
+	return fetch(`${apiURL}/snackToppings?snackId=${snackId}&_expand=topping`)
+	.then(response => response.json())
+	.then(parsedResponse => {
+		toppingCollection = parsedResponse
+		return parsedResponse;
+	})
+}
+
+export const getToppings = () => {
+	return fetch(`${apiURL}/toppings`)
 	.then(response => response.json())
 }
--- scripts/snacks/SnackDetails.js
+++ scripts/snacks/SnackDetails.js
@@ -1,13 +1,20 @@
 export const SnackDetails = (snackObject) => {
+	let toppingHTML = "";
+	snackObject.toppings.forEach(topping => {
+		toppingHTML += `${topping.topping.name}, `;
+	});
+
+	toppingHTML = toppingHTML.slice(0, -2);
+	
 	return `
 	<div class="col">
 		<div class="card shadow-sm" >
             <img class="bd-placeholder-img card-img-top"  style="max-width: 540px;" aria-label="Placeholder:${snackObject.name}" preserveAspectRatio="xMidYMid slice" focusable="false" src="${snackObject.snackImg}" alt="${snackObject.name}">
             <div class="card-body">
 				<h5 color="primary">${snackObject.name}</h5>
-              	<p class="card-text">${snackObject.description}</p>
-				 
-				  <div class="container">
+            	<p class="card-text">${snackObject.description}</p>
+
+				<div class="container">
 					<div class="row row-cols-2">
 						<div class="col col-details">Type: ${snackObject.type.name}</div>
 						<div class="col col-details">Shape: ${snackObject.shape.name}</div>
@@ -15,19 +22,62 @@ export const SnackDetails = (snackObject) => {
 						<div class="col col-details">Season: ${snackObject.season.name}</div>
 					</div>
 					<div class="row row-cols-1">
-						<div class="col col-details">${snackObject.toppings}</div>
+						<div class="col col-details">${toppingHTML}</div>
 					</div>
 				</div>
-			  	
+
 				<div class="d-flex justify-content-between align-items-center">
 					<div class="btn-group">
 					<button type="button" class="btn btn-sm btn-outline-secondary" id="editcake__${snackObject.id}" disabled>Edit</button>
 					<button type="button" class="btn btn-sm btn-outline-secondary" id="deletecake__${snackObject.id}" disabled>Delete</button>
 					</div>
                 	<small class="text-muted">Count: ${snackObject.count}</small>
-              	</div>
+            	</div>
             </div>
     	</div>
 	</div>
 	`
 }
+
+/* //! My Obsolete Function :( ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const SnackToppingList = (snackObject) => {
+	// An empty array to hold the topping ids.
+	let allSnackToppings = [];
+	// An emptry string to hold the names of all final toppings.
+	let displayedSnackToppings = "";
+	// Function to fetch all toppings of a single snack object, then...
+	getSingleSnackToppings(snackObject.id)
+		// With the object...
+		.then(snackToppingObj => {
+			// Look through each embeded topping object in an array, and with each topping object...
+			snackToppingObj.snackToppings.forEach(topping => {
+				// Push the id of the topping to the initially empty array, then...
+				allSnackToppings.push(topping.toppingId)
+			});
+			// Get all of the toppings possible, then...
+			getToppings()
+				// with the array...
+				.then(toppingArray => {
+					// Look through each topping object in the database, and...
+					toppingArray.forEach(toppingInList => {
+						// Look through each topping id in the filled snack topping array, and...
+						allSnackToppings.forEach(toppingInSingle => {
+							// If the ids of the snack and the topping object match...
+							if (toppingInList.id === toppingInSingle) {
+								// Add that topping's name to the string holding the names of all toppings, then...
+								displayedSnackToppings += `${toppingInList.name}, `
+							}
+						})
+					});
+					// Slice the final two characters from the completed string (', '), getting rid of the unecessary comma and whitespace.
+					displayedSnackToppings = displayedSnackToppings.slice(0, -2);
+					// Console log the result (it looks fine.)
+					console.log(displayedSnackToppings);
+					snackObject.toppings = displayedSnackToppings;
+					console.log(snackObject)
+					return snackObject;
+				})
+		})
+	return snackObject;
+}
+*/ //! ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
